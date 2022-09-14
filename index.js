1cd242f2c174aab8d7cd30733d47d5b601646e10
aleix-abuli <aleixabuli088@gmail.com>
2022-09-14 19:08:22 +0200

    section 2 w/o m queries

---
+++ index.js
@@ -0,0 +1,12 @@
+let mybutton = document.getElementById("scrollBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
